788660f26e2d9009534c88aa0dcf349e6362f55c
AndresP2Proyectos <acuta@unbosque.edu.co>
2025-05-21 00:40:12 -0500

    primer commit

---
+++ src/main/java/ISW1_ACT_AFCP_ASCC/ISW1_ACT_AFCP_ASCC/Isw1ActAfcpAsccApplication.java
@@ -0,0 +1,13 @@
+package ISW1_ACT_AFCP_ASCC.ISW1_ACT_AFCP_ASCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Isw1ActAfcpAsccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Isw1ActAfcpAsccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ISW1_ACT_AFCP_ASCC/ISW1_ACT_AFCP_ASCC/Isw1ActAfcpAsccApplicationTests.java
@@ -0,0 +1,13 @@
+package ISW1_ACT_AFCP_ASCC.ISW1_ACT_AFCP_ASCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Isw1ActAfcpAsccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
